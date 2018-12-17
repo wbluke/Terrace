f7d335c50929fea05b203bb1b07b05dfffeb3089
wbluke <pwb0127@naver.com>
2018-12-17 19:20:24 +0900

    set prerendering with vue.config.js in home directory

---
+++ frontend/home/vue.config.js
@@ -0,0 +1,26 @@
+/* vue.config.js */
+const path = require('path');
+const PrerenderSpaPlugin = require('prerender-spa-plugin');
+
+const productionPlugins = [
+  new PrerenderSpaPlugin({
+    staticDir: path.join(__dirname, 'dist'),
+    routes: [
+        "/",
+        "/blog",
+        "/daily"
+    ],
+    renderer: new PrerenderSpaPlugin.PuppeteerRenderer({
+      renderAfterElementExists: '#app'
+    }),
+  }),
+];
+
+module.exports = {
+  lintOnSave: false,
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(...productionPlugins);
+    }
+  },
+};
